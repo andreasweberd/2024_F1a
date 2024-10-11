260168561074d901e548d03dd912579e26e67101
DefaultAzubi <steffen.schuele14@web.de>
2024-10-11 14:31:39 +0200

    Update SoundSensor.ino

--- Schuele_Steffen/SoundSensor.ino
+++ Schuele_Steffen/SoundSensor.ino
@@ -1,13 +1,31 @@
-const int soundSensorPin = A0; // Pin connected to the sound sensor
+const int soundSensorPin = A0; 
+const int ledPins[] = {11, 12, 13}; // LEDs: 11 = grün, 12 = gelb, 13 = rot
 int soundLevel;
 
 void setup() {
-  Serial.begin(9600); // Start serial communication at 9600 baud rate
+  Serial.begin(9600); 
+  for (int i = 0; i < 3; i++) {
+    pinMode(ledPins[i], OUTPUT); 
+  }
 }
 
 void loop() {
-  soundLevel = analogRead(soundSensorPin); // Read the analog value from the sound sensor
-  Serial.print("Sound Level: ");
-  Serial.println(soundLevel); // Output the sound level to the serial monitor
-  delay(500); // Wait for half a second before the next reading
+  soundLevel = analogRead(soundSensorPin); 
+  Serial.println(soundLevel); 
+
+  if (soundLevel <= 100) {
+    digitalWrite(ledPins[0], HIGH); // Grüne LED an
+    digitalWrite(ledPins[1], LOW);  // Gelbe LED aus
+    digitalWrite(ledPins[2], LOW);  // Rote LED aus
+  } else if (soundLevel > 100 && soundLevel <= 200) {
+    digitalWrite(ledPins[0], LOW);  // Grüne LED aus
+    digitalWrite(ledPins[1], HIGH); // Gelbe LED an
+    digitalWrite(ledPins[2], LOW);  // Rote LED aus
+  } else if (soundLevel > 200) {
+    digitalWrite(ledPins[0], LOW);  // Grüne LED aus
+    digitalWrite(ledPins[1], LOW);  // Gelbe LED aus
+    digitalWrite(ledPins[2], HIGH); // Rote LED an
+  }
+
+  delay(500); 
 }
