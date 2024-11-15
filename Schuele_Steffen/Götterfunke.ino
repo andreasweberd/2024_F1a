6b871e02d1cc05d50e72f1c105374db2c7a9ee52
DefaultAzubi <steffen.schuele14@web.de>
2024-11-15 16:07:59 +0100

    Update Götterfunke.ino

--- Schuele_Steffen/Götterfunke.ino
+++ Schuele_Steffen/Götterfunke.ino
@@ -2,21 +2,59 @@
 
 int shortDuration = 250;
 int normalDuration = 500;
-int longDuration = 1000;
+int longDuration = 750;
+int extraLongDuration = 1000;
 
-int melodie[12][2] = {
-    {NOTE_E4, normalDuration},
-    {NOTE_E4, normalDuration},
-    {NOTE_F4, normalDuration},
-    {NOTE_G4, normalDuration},
-    {NOTE_F4, normalDuration},
-    {NOTE_E4, normalDuration},
-    {NOTE_D4, normalDuration},
-    {NOTE_C4, normalDuration},
-    {NOTE_C4, normalDuration},
-    {NOTE_D4, normalDuration},
-    {NOTE_E4, normalDuration},
-    {NOTE_E4, normalDuration}
+int melodie[62][2] = {
+  {NOTE_E4, normalDuration}, {NOTE_E4, normalDuration}, 
+  {NOTE_F4, normalDuration}, {NOTE_G4, normalDuration}, //takt 1
+
+  {NOTE_G4, normalDuration}, {NOTE_F4, normalDuration}, 
+  {NOTE_E4, normalDuration}, {NOTE_D4, normalDuration}, //takt 2 
+
+  {NOTE_C4, normalDuration}, {NOTE_C4, normalDuration}, 
+  {NOTE_D4, normalDuration}, {NOTE_E4, normalDuration}, //takt 3  
+
+  {NOTE_E4, longDuration}, {NOTE_D4, shortDuration}, 
+  {NOTE_D4, extraLongDuration}, // takt 4 vers 1 ende
+
+  {NOTE_E4, normalDuration}, {NOTE_E4, normalDuration}, 
+  {NOTE_F4, normalDuration}, {NOTE_G4, normalDuration}, //takt 5 ende
+
+  {NOTE_G4, normalDuration}, {NOTE_F4, normalDuration}, 
+  {NOTE_E4, normalDuration}, {NOTE_D4, normalDuration}, //takt 6 ende
+
+  {NOTE_C4, normalDuration}, {NOTE_C4, normalDuration}, 
+  {NOTE_D4, normalDuration}, {NOTE_E4, normalDuration}, //takt 7 ende 
+
+  {NOTE_D4, longDuration}, {NOTE_C4, shortDuration}, 
+  {NOTE_C4, extraLongDuration}, // takt 8 vers 2 ende
+
+  {NOTE_D4, normalDuration}, {NOTE_D4, normalDuration}, 
+  {NOTE_E4, normalDuration}, {NOTE_C4, normalDuration}, //takt 9 ende
+
+  {NOTE_D4, normalDuration}, {NOTE_E4, shortDuration},
+  {NOTE_F4, shortDuration}, {NOTE_E4, normalDuration},
+  {NOTE_C4, normalDuration}, //takt 10 ende
+
+  {NOTE_D4, normalDuration}, {NOTE_E4, shortDuration}, 
+  {NOTE_F4, shortDuration}, {NOTE_E4, normalDuration}, 
+  {NOTE_D4, normalDuration}, //takt 11 ende
+
+  {NOTE_C4, normalDuration}, {NOTE_D4, normalDuration},
+  {NOTE_G3, normalDuration}, {NOTE_E4, normalDuration}, //takt 12 ende vers 3 ende
+
+  {NOTE_E4, normalDuration}, {NOTE_F4, normalDuration},
+  {NOTE_G4, normalDuration}, //takt 13 ende
+
+  {NOTE_G4, normalDuration}, {NOTE_F4, normalDuration},
+  {NOTE_E4, normalDuration}, {NOTE_D4, normalDuration}, //takt 14 ende
+
+  {NOTE_C4, normalDuration}, {NOTE_C4, normalDuration},
+  {NOTE_D4, normalDuration}, {NOTE_E4, normalDuration}, //takt 15 ende
+
+  {NOTE_D4, longDuration}, {NOTE_C4, shortDuration},
+  {NOTE_C4, extraLongDuration} // takt 16 ende vers 4 ende lied ende
 };
 
 void setup() {
@@ -24,9 +62,11 @@ void setup() {
 }
 
 void loop() {
-    for (int i = 0; i < 12; i++) {
-        tone(8, melodie[i][0], melodie[i][1]);
-        delay(melodie[i][1]);
-        noTone(8);
-    }
+  for (int i = 0; i < 62; i++) {
+      tone(8, melodie[i][0], melodie[i][1]);
+      delay(melodie[i][1] * 0.8);
+      noTone(8);
+      delay(melodie[i][1] * 0.2);
+  }
+  delay(9000);
 }
